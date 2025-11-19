5a4e7fdcbd11f23d4535fbdec82f705306605f1e
Tomás <tommyengonga10@gmail.com>
2025-11-19 04:30:58 +0100

    3

--- src/main/java/com/ohhell/ohhellapi/HelloApplication.java
+++ src/main/java/com/ohhell/ohhellapi/HelloApplication.java
@@ -2,8 +2,16 @@ package com.ohhell.ohhellapi;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import java.util.Set;
 
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
-
+    
+    @Override
+    public Set<Class<?>> getClasses() {
+        return Set.of(
+            com.ohhell.ohhellapi.resources.HelloResource.class,
+            com.ohhell.ohhellapi.resources.TestDatabaseResource.class
+        );
+    }
 }
